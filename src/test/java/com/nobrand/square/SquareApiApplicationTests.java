31204e7c6ed4f8bc812151c5e41f908665be9e00
no-brand <do.dream.david@gmail.com>
2021-11-01 22:46:12 +0900

    Create Base Project

---
+++ src/test/java/com/nobrand/square/SquareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nobrand.square;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
